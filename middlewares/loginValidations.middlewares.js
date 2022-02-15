210700b331c0df58890be63a77a6161d9ba5646e
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-15 11:59:26 -0300

    feat: adicionado validação se o e-mail possui formato correto ao post /login

--- middlewares/loginValidations.middlewares.js
+++ middlewares/loginValidations.middlewares.js
@@ -1,6 +1,9 @@
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 const emailValidation = (req, _res, next) => {
   const { email } = req.body;
   if (!email) return next('emailEmpty');
+  if (!emailRegexp.test(email)) return next('emailInvalid');
   return next();
 };
 
