945c92f279571b92961cd04e48f2aa89b5b94d14
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-15 13:03:45 -0300

    refactor: refatorado middleware talkerValidations

--- middlewares/talkerValidations.middleware.js
+++ middlewares/talkerValidations.middleware.js
@@ -1,16 +1,33 @@
 const crypto = require('crypto');
 
+const nameValidation = (name, next) => {
+  const MIN_CHAR = 3;
+
+  if (!name) return next('nameEmpty');
+  if (name.length <= MIN_CHAR) return next('nameInvalid');
+  return false;
+};
+
+const ageValidation = (age, next) => {
+  const MIN_AGE = 18;
+
+  if (!age) return next('ageEmpty');
+  if (age < MIN_AGE) return next('ageInvalid');
+  return false;
+};
+
+const tokenValidation = (authorization, next) => {
+  if (authorization.length !== 16) return next('tokenInvalid');
+  return false;
+};
+
 const talkerValidationPost = (req, _res, next) => {
   const { name, age, talk } = req.body;
   const { authorization } = req.headers;
-  const MIN_CHAR = 3;
-  const MIN_AGE = 18;
 
-  if (authorization.length !== 16) return next('tokenInvalid');
-  if (!name) return next('nameEmpty');
-  if (name.length <= MIN_CHAR) return next('nameInvalid');
-  if (!age) return next('ageEmpty');
-  if (age < MIN_AGE) return next('ageInvalid');
+  tokenValidation(authorization, next);
+  nameValidation(name, next);
+  ageValidation(age, next);
   return next();
 };
 
